69729ab7c2ed0a23cc619884beed1b06d0c4f797
kalika15 <86293635+kalika15@users.noreply.github.com>
2022-09-01 21:16:37 +0530

    Update urls.py

--- Hello/urls.py
+++ Hello/urls.py
@@ -25,7 +25,6 @@ admin.site.index_title = "Welcome to KIW "
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('home.urls'))
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
+   
     
 ]
